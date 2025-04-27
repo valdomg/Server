d736fffd4529dd785d2da4b4cdace4c8aa15c44d
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-04-27 14:32:31 -0300

    Implementação de IRQ

--- Server.c
+++ Server.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
+#include "Library/IRQ.h"
 
 
 int main()
 {
     stdio_init_all();
+
+    setupBTN();
+    gpio_set_irq_enabled_with_callback(BTN_A, GPIO_IRQ_EDGE_RISE, true, &resetIRQ);
+
+    
 
     while (true) {
         printf("Hello, world!\n");
