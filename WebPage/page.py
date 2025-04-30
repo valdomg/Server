23073735c87e44877f1973b2d7637c474d8acb29
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-04-30 16:14:58 -0300

    Exclusão de rota

--- WebPage/page.py
+++ WebPage/page.py
@@ -5,30 +5,25 @@ import logging
     SERVIDOR COM PYTHON UTILIZANDO O FRAMEWORK FLASK 
 '''
 app = Flask(__name__)
+
 logging.basicConfig(level=logging.DEBUG)
 coordenadas = []
 
 @app.route("/")
 def index():
-
     x = request.args.get('x', type=int)
     y = request.args.get('y', type=int)
     coordenadas.append((x,y))
+
     print("X: ", x, "y: ", y)
+
+    #if x is not None and y is not None:
+     #   coordenadas.append(("Valor não encontrado", "Valor não encontrado")) 
 
     ultima = coordenadas[-2]
     print(ultima)
 
     return rd("index.html", ultima = ultima)
-
-@app.route('/coordenadas')
-def receber_coordenadas():
-    x = request.args.get('x', type=int)
-    y = request.args.get('y', type=int)
-    print(f"Recebido: x = {x}, y = {y}")
-
-    return "Recebido"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded= True)
