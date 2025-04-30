4bec6ef754ad7dfce44a6d98ccb5bd36c11a4b18
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-04-30 11:28:33 -0300

    Remoção de uso de buffer

--- Server.c
+++ Server.c
@@ -16,7 +16,7 @@ const char WIFI_SSID [20] = "Online.Rosa";
 const char WIFI_PASS [20] = "3914902323";
 
 const char HOST [20] = "192.168.1.19";
-const uint PORT = 80;
+const uint PORT = 5000;
 
 
 #define BUFFER_SIZE 10
@@ -46,29 +46,26 @@ void adicionar_ao_buffer(uint x, uint y) {
 
 // Tenta enviar o primeiro item do buffer
 void enviar_do_buffer() {
-    if (buffer_inicio == buffer_fim) {
-        // buffer vazio
-        return;
-    }
 
-    Coordenada *c = &buffer[buffer_inicio];
+    uint x = read_adc_x();
+    uint y = read_adc_y();
 
     char path[100];
-    snprintf(path, sizeof(path), "/?x=%u&y=%u", c->x, c->y);
+    snprintf(path, sizeof(path), "/?x=%u&y=%u", x, y);
 
     EXAMPLE_HTTP_REQUEST_T req = {0};
     req.hostname = HOST;
     req.url = path;
     req.port = PORT;
-    req.headers_fn = http_client_header_print_fn;
-    req.recv_fn = http_client_receive_print_fn;
+    //req.headers_fn = Null;
+    //req.recv_fn = http_client_receive_print_fn;
 
     printf("Enviando: %s\n", path);
     int result = http_client_request_sync(cyw43_arch_async_context(), &req);
 
     if (result == 0) {
         printf("Enviado com sucesso!\n");
-        buffer_inicio = (buffer_inicio + 1) % BUFFER_SIZE; // remove do buffer
+        //buffer_inicio = (buffer_inicio + 1) % BUFFER_SIZE; // remove do buffer
     } else {
         printf("Erro ao enviar (%d), tentando novamente depois.\n", result);
         // Não avança o buffer — tentativa futura
@@ -77,15 +74,9 @@ void enviar_do_buffer() {
     sleep_ms(100);
 }
 
-int main()
-{
-    stdio_init_all();
 
-    setupBTN();
-    gpio_set_irq_enabled_with_callback(BTN_A, GPIO_IRQ_EDGE_RISE, true, &resetIRQ);
-    setupJoy();
-    // Inicializa Wi-Fi
 
+void conectarWifi(){
     if (cyw43_arch_init()){
         printf("Erro ao inicializar o Wi-Fi\n");
     }
@@ -101,19 +92,23 @@ int main()
         uint8_t *ip_address = (uint8_t *)&(cyw43_state.netif[0].ip_addr.addr);
         printf("Wi-Fi conectado! IP: %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
     }
+}
+
+int main()
+{
+    stdio_init_all();
+
+    setupBTN();
+    gpio_set_irq_enabled_with_callback(BTN_A, GPIO_IRQ_EDGE_RISE, true, &resetIRQ);
+    setupJoy();
+    // Inicializa Wi-Fi
+    conectarWifi();
     
     
     while (true){
-        
-        uint x = read_adc_x();
-        uint y = read_adc_y();
-
-        adicionar_ao_buffer(x, y);
-
         enviar_do_buffer();
-
-        sleep_ms(1000);
-
+        cyw43_arch_poll();
+        sleep_ms(2000);
     }
     
 }
