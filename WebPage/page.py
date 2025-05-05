05c20bee60ad1f7cc3522edee51a18118ff9f1f0
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-05-05 15:09:21 -0300

    Uso de socketio para atualizar a posição do joystick

--- WebPage/page.py
+++ WebPage/page.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask import render_template as rd
+from flask_socketio import SocketIO, emit
 import logging
 '''
     SERVIDOR COM PYTHON UTILIZANDO O FRAMEWORK FLASK 
@@ -7,23 +8,32 @@ import logging
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
-coordenadas = []
+socketio = SocketIO(app)
+
+coordenadas = (0,0)
 
 @app.route("/")
 def index():
     x = request.args.get('x', type=int)
     y = request.args.get('y', type=int)
-    coordenadas.append((x,y))
 
     print("X: ", x, "y: ", y)
 
-    #if x is not None and y is not None:
-     #   coordenadas.append(("Valor não encontrado", "Valor não encontrado")) 
+    return rd("index.html")
 
-    ultima = coordenadas[-2]
-    print(ultima)
 
-    return rd("index.html", ultima = ultima)
+@app.route("/coordenadas")
+def receber_coordenadas():
+    global coordenada
+    x = request.args.get('x', type=int)
+    y = request.args.get('y', type=int)
+
+    if x is not None and y is not None:
+        coordenada = (x, y)
+        print(f"Recebido: x = {x}, y = {y}")
+        socketio.emit('nova_coordenada', {'x': x, 'y': y})
+    return "OK"
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded= True)
+     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
