db77de5948ba849975e774344c19bc526d37e4da
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-05-10 15:03:38 -0300

    Implementação de uso dos outros botões

--- Library/IRQ.h
+++ Library/IRQ.h
@@ -4,12 +4,26 @@
 
 const int BTN_A = 5;
 
+const int BTN_B = 6;
+
+const int BTN_JOY = 22;
+
 void setupBTN(){
     gpio_init(BTN_A);
     gpio_set_dir(BTN_A, GPIO_IN);
     gpio_pull_up(BTN_A);
 
+    gpio_init(BTN_B);
+    gpio_set_dir(BTN_B, GPIO_IN);
+    gpio_pull_up(BTN_B);
+
+    gpio_init(BTN_JOY);
+    gpio_set_dir(BTN_JOY, GPIO_IN);
+    gpio_pull_up(BTN_JOY);
+
 }
+
+
 
 void resetIRQ(uint gpio, uint32_t events){
     reset_usb_boot(0, 0);
